143783df2aab5e645337a2f07e0011cd8d51777b
Orion <Orion45.dd@gmail.com>
2024-05-01 11:52:42 -0700

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""API routes and functionality here"""
 
 from flask import Flask
 from models import storage
@@ -7,12 +8,20 @@ from os import getenv
 
 app = Flask(__name__)
 
+# Register blueprint
 app.register_blueprint(app_views)
 
+
+# app context
 @app.teardown_appcontext
-def teardown(self):
+def teardown_context(exception):
     storage.close()
 
+
 if __name__ == "__main__":
-    app.run(host=getenv("HBNB_API_HOST", '0.0.0.0'),
-            port=int(getenv("HBNB_API_PORT", 5000)), treaded=True)
+    # Configure host and Port
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.getenv("HBNB_API_PORT", 5000))
+
+# Run the Flask server
+    app.run(host=host, port=port, treaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,7 @@
 from flask import jsonify
 from api.v1.views import app_views
 
-@app_views.route('/status')
-def api_status():
 
+@app_views.route('/status')
+def get_status():
     return jsonify({'status': "OK"})
